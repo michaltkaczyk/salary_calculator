11af034edadd3860ec9c906127d75bea0bf61942
michaltkaczyk <michaltkaczyk@outlook.com>
2018-12-22 08:29:37 +0100

    Rename all variables to English, add health insurance constribution
    Rename all variables to English, add calculatuions health insurance constribution

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 def gross_to_net(gross_wage):
-    _skladka_emerytalna_percentage = 0.0976
-    _skladka_rentowa_percentage = 0.0150
-    _skladka_chorobowa_percentage = 0.0245
 
-    skladka_emerytalna = gross_wage * _skladka_emerytalna_percentage
-    skladka_rentowa = gross_wage * _skladka_rentowa_percentage
-    skladka_chorobowa = gross_wage * _skladka_chorobowa_percentage
+    _pension_insurance_contribution_percentage = 0.0976
+    _disability_pension_insurance_contribution_percentage = 0.0150
+    _sickness_insurance_contribution_percentage = 0.0245
+    _health_insurance_contribution_percentage = 0.0900
+
+    pension_insurance_contribution = gross_wage * _pension_insurance_contribution_percentage
+    disability_pension_insurance_contribution = gross_wage * _disability_pension_insurance_contribution_percentage
+    sickness_insurance_contribution = gross_wage * _sickness_insurance_contribution_percentage
+
+    social_insurance_contribution =\
+        pension_insurance_contribution + disability_pension_insurance_contribution + sickness_insurance_contribution
+
+    health_insurance_contribution =\
+        (gross_wage - social_insurance_contribution) * _health_insurance_contribution_percentage
 
     print("Wynagrodzenie brutto:", "{:.2f}".format(gross_wage), sep=" ")
-
+    print()
     print("Składki na ubezpieczenia społeczne:")
-    print("Ubezpieczenie emerytalne:", "{:.2f}".format(skladka_emerytalna), sep=" ")
-    print("Ubezpieczenie rentowe:", "{:.2f}".format(skladka_rentowa), sep=" ")
-    print("Ubezpieczenie chorobowe:", "{:.2f}".format(skladka_chorobowa), sep=" ")
+    print("\tUbezpieczenie emerytalne:", "{:.2f}".format(pension_insurance_contribution), sep=" ")
+    print("\tUbezpieczenie rentowe:", "{:.2f}".format(disability_pension_insurance_contribution), sep=" ")
+    print("\tUbezpieczenie chorobowe:", "{:.2f}".format(sickness_insurance_contribution), sep=" ")
+    print("Suma składek na ubezpieczenia społeczne:", "{:.2f}".format(social_insurance_contribution), sep=" ")
+    print()
+    print("Składka na ubezpieczenia społeczne:", "{:.2f}".format(health_insurance_contribution), sep=" ")
