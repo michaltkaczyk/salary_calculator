bafac3003a7f60bce28d96d2be4cfdd8901db4c4
michaltkaczyk <michaltkaczyk@outlook.com>
2018-12-23 14:51:31 +0100

    Add same_town arg to calculate tax deductible expenses
    Arg same_town added to calculate tax deductible expenses. Dict output added.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def gross_to_net(gross_wage):
+def gross_to_net(gross_wage, same_town=True):
 
     _pension_insurance_contribution_percentage = 0.0976
     _disability_pension_insurance_contribution_percentage = 0.0150
     _sickness_insurance_contribution_percentage = 0.0245
     _health_insurance_contribution_percentage = 0.0900
 
+    _same_town_employee_deduction = 111.25
+    _different_town_employee_deduction = 139.06
+
+    # Składki na ubezpieczenia społeczne
     pension_insurance_contribution = gross_wage * _pension_insurance_contribution_percentage
     disability_pension_insurance_contribution = gross_wage * _disability_pension_insurance_contribution_percentage
     sickness_insurance_contribution = gross_wage * _sickness_insurance_contribution_percentage
@@ -12,8 +16,14 @@ def gross_to_net(gross_wage):
     social_insurance_contribution =\
         pension_insurance_contribution + disability_pension_insurance_contribution + sickness_insurance_contribution
 
-    health_insurance_contribution =\
-        (gross_wage - social_insurance_contribution) * _health_insurance_contribution_percentage
+    # Przychód
+    income = gross_wage - social_insurance_contribution
+
+    # Składka na ubezpieczenie zdrowotne
+    health_insurance_contribution = income * _health_insurance_contribution_percentage
+
+    # Koszty uzyskania przychodu:
+    tax_deductible_expences = _same_town_employee_deduction if same_town else _different_town_employee_deduction
 
     print("Wynagrodzenie brutto:", "{:.2f}".format(gross_wage), sep=" ")
     print()
@@ -23,4 +33,12 @@ def gross_to_net(gross_wage):
     print("\tUbezpieczenie chorobowe:", "{:.2f}".format(sickness_insurance_contribution), sep=" ")
     print("Suma składek na ubezpieczenia społeczne:", "{:.2f}".format(social_insurance_contribution), sep=" ")
     print()
-    print("Składka na ubezpieczenia społeczne:", "{:.2f}".format(health_insurance_contribution), sep=" ")
+    print("Składka na ubezpieczenie zdrowotne:", "{:.2f}".format(health_insurance_contribution), sep=" ")
+
+    summary = {
+        'Pension Insurance Contribution': pension_insurance_contribution,
+        'Disability Pension Insurance Contribution': disability_pension_insurance_contribution,
+        'Sickness Insurance Contribution': sickness_insurance_contribution,
+        'Health Insurance Contribution': health_insurance_contribution}
+
+    return summary
